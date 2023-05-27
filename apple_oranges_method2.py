bbdb880c1ed6b8284ac3251f186e525dd27bf3cf
Suraj Singh <50705716+naurang@users.noreply.github.com>
2023-05-27 17:09:02 +0530

    Create apple_oranges_method2.py

---
+++ apple_oranges_method2.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    c = 0
+    d = 0
+    for i in apples: 
+        i = i + a   # this line adds the position of a in apples unit
+        if s<= i and i <=t:  #this check if the units falls under house range
+            c += 1           # this line counts the units that fall under house range
+    for j in oranges: 
+        j = j + b
+        if s<= j and j <=t:
+            d += 1 
+    print(c)
+    print(d)
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
